595f354d19e8707fe3808edcaee1add8c3f4a01f
santtose <santtose@outlook.com>
2020-06-09 16:10:37 -0300

    Abrindo o arquivo

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -699,7 +699,7 @@ class DropBoxController {
                     break;
 
                 default:
-                    window.open('/file?path=' + file.path);
+                    window.open(file.path);
 
             }
 
